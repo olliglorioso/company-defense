0ec13bb606c9aaf81e3f27f1c7665ccff9808a0b
Olli <olli.glorioso@gmail.com>
2023-03-30 00:45:14 +0300

    Some progress on the dragging

--- src/main/scala/Logic/Tower.scala
+++ src/main/scala/Logic/Tower.scala
@@ -1,3 +1,21 @@
 package Logic
 import Logic.*
-case class Tower (path: String, size: Int, name: String) extends GameObject(path, size, name) {}
+import scalafx.Includes.jfxMouseEvent2sfx
+import scalafx.scene.input.InputIncludes.jfxMouseEvent2sfx
+
+case class Tower (path: String, size: Int, name: String) extends GameObject(path, size, name) {
+    onMousePressed = (event) => {
+      val x = event.sceneX - layoutX()
+      val y = event.sceneY - layoutY()
+      translateX = 0
+      println(x)
+      onMouseDragged = (event) => {
+        translateX = event.sceneX - x
+        translateY = event.sceneY - y
+        println(layoutX)
+      }
+      onMouseReleased = (event) => {
+      }
+    }
+  
+}
